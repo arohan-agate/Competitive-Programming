922cabe183a1a79cd2dee9912644517b7f709b3b
Arohan Agate <arogate05@gmail.com>
2021-08-11 20:04:03 -0700

    Create domino_piling.java

---
+++ CodeForces/domino_piling.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class domino_piling {
+	
+	public static void main (String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int d1 = scan.nextInt();
+		int d2 = scan.nextInt();
+		
+		System.out.println(d1*d2/2);
+		
+	}
+	
+}
